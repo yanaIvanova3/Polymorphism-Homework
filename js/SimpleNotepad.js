1276b860e028e71b83b553c73a38a15759507d98
Yana Ivanova <y.m.ivanova23@gmail.com>
2016-02-20 23:47:12 +0200

    SimpleNotepad > module

--- js/SimpleNotepad.js
+++ js/SimpleNotepad.js
@@ -1,45 +1,49 @@
 /**
  * Created by Yana on 2/19/2016.
  */
-function SimpleNotepad (notepadPages) {
-    AbstractNotepad.call(this, notepadPages);
-}
 
-SimpleNotepad.prototype = Object.create(AbstractNotepad.prototype);
-SimpleNotepad.prototype.constructor = SimpleNotepad;
+var simpleNotepad = (function create(parent) {
 
-SimpleNotepad.prototype.addText = function(page, text) {
-    var selectedPage = this.getPages().indexOf(page);
-    this.getPages()[selectedPage].addTextToPage(text);
-}
-
-SimpleNotepad.prototype.deleteText = function (page) {
-    var selectedPage = this.getPages().indexOf(page);
-    this.getPages()[selectedPage].deleteTextFromPage();
-}
-
-SimpleNotepad.prototype.replaceText = function (page, text) {
-    var selectedPage = this.getPages().indexOf(page);
-    this.getPages()[selectedPage].setText(text);
-}
-
-SimpleNotepad.prototype.viewPages = function () {
-    var pages = this.getPages();
-    for (var i = 0; i < pages.length; i++) {
-       console.log(pages[i].viewPageInfo());
+    function SimpleNotepad(notepadPages) {
+        parent.call(this, notepadPages);
     }
-}
 
-SimpleNotepad.prototype.searchInPages = function (word) {
-    var pages = this.getPages();
-    for (var i = 0; i < pages.length; i++) {
-        pages[i].searchWord(word);
-    }
-}
+    SimpleNotepad.prototype = Object.create(parent.prototype);
+    SimpleNotepad.prototype.constructor = SimpleNotepad;
 
-SimpleNotepad.prototype.printAllPagesWithDigits = function () {
-    var pages = this.getPages();
-    for (var i = 0; i < pages.length; i++) {
-        pages[i].hasDigits();
+    SimpleNotepad.prototype.addText = function (page, text) {
+        var selectedPage = this.getPages().indexOf(page);
+        this.getPages()[selectedPage].addTextToPage(text);
     }
-}
+
+    SimpleNotepad.prototype.deleteText = function (page) {
+        var selectedPage = this.getPages().indexOf(page);
+        this.getPages()[selectedPage].deleteTextFromPage();
+    }
+
+    SimpleNotepad.prototype.replaceText = function (page, text) {
+        var selectedPage = this.getPages().indexOf(page);
+        this.getPages()[selectedPage].setText(text);
+    }
+
+    SimpleNotepad.prototype.viewPages = function () {
+        var pages = this.getPages();
+        for (var i = 0; i < pages.length; i++) {
+            console.log(pages[i].viewPageInfo());
+        }
+    }
+
+    SimpleNotepad.prototype.searchInPages = function (word) {
+        var pages = this.getPages();
+        for (var i = 0; i < pages.length; i++) {
+            pages[i].searchWord(word);
+        }
+    }
+
+    SimpleNotepad.prototype.printAllPagesWithDigits = function () {
+        var pages = this.getPages();
+        for (var i = 0; i < pages.length; i++) {
+            pages[i].hasDigits();
+        }
+    }
+}(AbstractNotepad));
